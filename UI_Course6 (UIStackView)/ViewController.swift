b718376238d4f741580ebb8e2c71ee34a50357fa
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-12 17:04:49 +0300

    Initial Commit

---
+++ UI_Course6 (UIStackView)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Course6 (UIStackView)
+//
+//  Created by Александр Тарасов on 12/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
